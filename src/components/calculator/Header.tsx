c3dd685c79c52d92925d625ace80a4af24ac9824
Pedro Brun <pedroluccavb@gmail.com>
2023-08-24 18:51:22 -0300

    feat: handle dynamic height in header

--- src/components/calculator/Header.tsx
+++ src/components/calculator/Header.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import { useEffect, useRef, useState } from "react";
 
-const Header = ({ value }: { value: string | number }) => {
+export function Header({ value }: { value: string }) {
+  const [scale, setScale] = useState(1);
+  const spanRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const parent = spanRef.current?.parentNode as HTMLElement;
+    const availableWidth = parent?.offsetWidth;
+    const actualWidth = spanRef.current?.offsetWidth;
+    const actualScale = availableWidth / (actualWidth || 0);
+    if (scale !== actualScale) {
+      if (actualScale < 1) {
+        setScale(actualScale);
+      }
+      if (scale < 1) {
+        setScale(1);
+      }
+    }
+  }, [value, scale]);
+
   return (
-    <header className="flex justify-end items-end pb-4 pr-5 relative">
+    <header className="w-72 h-36 flex justify-end items-end pb-4 pr-5 relative">
       <strong
         className="text-white text-7xl font-light absolute right-0 origin-right"
+        ref={spanRef}
+        style={{ transform: `scale(${scale},${scale})` }}
       >
         {value}
       </strong>
     </header>
-  )
+  );
 }
 
 export default Header

0129bb819351d436773da418cb2b384b76a35e21
Pedro Brun <pedroluccavb@gmail.com>
2023-08-24 23:45:57 -0300

    feat: reduce calculator header height

--- src/components/calculator/Header.tsx
+++ src/components/calculator/Header.tsx
@@ -20,7 +20,7 @@ export function Header({ value }: { value: string }) {
   }, [value, scale]);
 
   return (
-    <header className="w-72 h-36 flex justify-end items-end pb-4 pr-5 relative">
+    <header className="w-72 h-12 flex justify-end items-end pb-4 pr-5 relative">
       <strong
         className="text-white text-7xl font-light absolute right-0 origin-right"
         ref={spanRef}

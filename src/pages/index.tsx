ddb1320a8e96309254e4a9b6971b7b388b1a912c
Pedro Brun <pedroluccavb@gmail.com>
2023-08-28 08:22:23 -0300

    feat: add page title to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Calculator from "@/components/calculator";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center w-full min-h-screen bg-black overflow-y-auto">
+      <Head>
+        <title>Calculator</title>
+      </Head>
       <Calculator />
     </main>
   );
